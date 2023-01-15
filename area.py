0eb21a7164032df16f6f94e1c4db609200276167
bhattathakur <bhattathaur2015@gmail.com>
2023-01-15 17:23:37 -0500

    updated /home/thakur/simulation_analysis on Sun Jan 15 05:23:37 PM EST 2023

---
+++ area.py
@@ -0,0 +1,32 @@
+# Assign a value to radius
+import math
+#this library has many important math fuction
+#radius=float(input("Enter the radius of a circle:"))
+##radius = 20 # radius is now 20 radius 20
+#
+## Compute area
+##area = radius * radius * 3.14159 
+#
+#area = radius **2 *  3.14159 
+#perimeter=2*3.14*radius
+#
+## Display results
+#print("The area for the circle of radius", radius, "is", area)
+#print("The perimeter for the circle of radius", radius, "is", perimeter)
+
+
+#for i in range(1,100000):
+#    print("radius: ",i, "area:", 3.14*i*i,"perimeter:",2*3.14*i)
+#    #print("radius: "+i, "area:"+ 3.14*i*i+"perimeter:",2*3.14*i)
+
+name=input("Enter your name :")
+
+print("hello ",name, "!")
+
+length=eval(input("Enter the lenght of a rectangle:"))
+breadth=eval(input("Enter the breadth of a rectangle:"))
+
+area=length*breadth
+perimeter=2*(length+breadth)
+
+print("area: ",area, "perimeter:", perimeter)
